b578f4c15ad0c0b5b7d7be9a9fe374dc4350bf52
Alejandro Echeverria <alejandro@echev.com>
2016-08-10 19:12:21 -0400

    Scene progression

--- code/raytracer/python/app.py
+++ code/raytracer/python/app.py
@@ -6,7 +6,7 @@ from display import display
 
 def render():
     resources = loadResources("resources/resources.json")
-    scene = loadScene("scenes/test_scene_3.json")
+    scene = loadScene("scenes/test_scene_2.json")
     width = 512
     height = 512
     image = rayTrace(scene, resources, width, height)
