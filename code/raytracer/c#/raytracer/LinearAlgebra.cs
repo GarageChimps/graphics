acaf2fcaf473165fe0fbe468ba905c320833e42d
Félix Fischer <felix91gr@gmail.com>
2016-08-12 10:20:07 -0400

    Implementado struct Vector
    Programa compila y renderea sin problemas. 
    Falta integrarlo con el programa y sacar las Listas de floats.

--- code/raytracer/c#/raytracer/LinearAlgebra.cs
+++ code/raytracer/c#/raytracer/LinearAlgebra.cs
@@ -3,6 +3,125 @@ using System.Collections.Generic;
 
 namespace raytracer
 {
+  struct Vector
+  {
+    /// <summary>
+    /// Magnitude of the vector
+    /// </summary>
+    public float Size
+    {
+      get { return (float)Math.Sqrt(SizeSq); }
+    }
+
+    /// <summary>
+    /// Size squared for fast magnitude comparison
+    /// </summary>
+    public float SizeSq
+    {
+      get { return this ^ this; }
+    }
+
+    /// <summary>
+    /// A normalized version of this vector
+    /// </summary>
+    public Vector Normalized
+    {
+      get { return this / Size; }
+    }
+
+    public float R, G, B;
+
+    public float X
+    {
+      get { return R; }
+      set { R = value; }
+    }
+    public float Y
+    {
+      get { return G; }
+      set { G = value; }
+    }
+    public float Z
+    {
+      get { return B; }
+      set { B = value; }
+    }
+
+    Vector(float R, float G, float B)
+    {
+      this.R = R;
+      this.G = G;
+      this.B = B;
+    }
+
+    public static Vector operator +(Vector vector1, Vector vector2)
+    {
+      return new Vector(vector1.R + vector2.R, vector1.G + vector2.G, vector1.B + vector2.B);
+    }
+
+    public static Vector operator +(Vector vector1, float scalar)
+    {
+      return new Vector(vector1.R + scalar, vector1.G + scalar, vector1.B + scalar);
+    }
+
+    public static Vector operator -(Vector vector1, Vector vector2)
+    {
+      return new Vector(vector1.R - vector2.R, vector1.G - vector2.G, vector1.B - vector2.B);
+    }
+
+    public static Vector operator -(Vector vector1, float scalar)
+    {
+      return new Vector(vector1.R - scalar, vector1.G - scalar, vector1.B - scalar);
+    }
+
+    public static Vector operator *(Vector vector1, Vector vector2)
+    {
+      return new Vector(vector1.R * vector2.R, vector1.G * vector2.G, vector1.B * vector2.B);
+    }
+
+    public static Vector operator *(Vector vector1, float scalar)
+    {
+      return new Vector(vector1.R * scalar, vector1.G * scalar, vector1.B * scalar);
+    }
+
+    public static Vector operator /(Vector vector1, Vector vector2)
+    {
+      return new Vector(vector1.R / vector2.R, vector1.G / vector2.G, vector1.B / vector2.B);
+    }
+    
+    public static Vector operator /(Vector vector1, float scalar)
+    {
+      return new Vector(vector1.R / scalar, vector1.G / scalar, vector1.B / scalar);
+    }
+
+    /// <summary>
+    /// Dot product between 2 vectors
+    /// </summary>
+    /// <param name="vector1"></param>
+    /// <param name="vector2"></param>
+    /// <returns></returns>
+    public static float operator ^(Vector vector1, Vector vector2)
+    {
+      return vector1.R * vector2.R + vector1.G * vector2.G + vector1.B * vector2.B;
+    }
+
+    /// <summary>
+    /// Cross product between 2 vectors
+    /// </summary>
+    /// <param name="vector1"></param>
+    /// <param name="vector2"></param>
+    /// <returns></returns>
+    public static Vector operator %(Vector vector1, Vector vector2)
+    {
+      float R = vector1.G * vector2.B - vector2.G * vector1.B;
+      float G = vector2.R * vector1.B - vector1.R * vector2.B;
+      float B = vector1.R * vector2.G - vector2.R * vector1.G;
+
+      return new Vector(R, G, B);
+    }
+
+  }
+
   //Linear Algebra Lib
   static class LinearAlgebra
   {
